9c6bbeae0ace4d3d771d9067926393553e94848a
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-05-04 09:26:49 +0000

    falmes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import FlamesApp from "./flames";
 
 class App extends Component {
     render() {
 
         return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
+            <>
+               <FlamesApp/>
+            </>
         )
     }
 }
